33d5fb27d59cb3b82a18a11d6aa11b4e8cc163bc
Araggorn <zerelik@gmail.com>
2020-11-20 21:11:47 +0100

    v3Done

--- src/main/java/pl/coderslab/app/Book.java
+++ src/main/java/pl/coderslab/app/Book.java
@@ -2,9 +2,11 @@ package pl.coderslab.app;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class Book {
     private Long id;
     private String isbn;
